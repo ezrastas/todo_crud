a378f94152a2a297c364ad24345e9b64ee2a2eda
Stanislav Ezra Babushkin <playmarketstas@gmail.com>
2018-03-04 17:18:11 +0300

    text filter added in todolist container

--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -7,21 +7,34 @@ import TodoPanel from './TodoPanel';
 export default class TodoList extends React.PureComponent {
   constructor(props) {
     super(props);
+    this.state = {
+      searchtext: undefined,
+    }
   }
 
+  HandleSearchText = value => this.setState({ searchtext: value });
+
   render() {
-    const todos = this.props.todos;
+    //const todos = this.props.todos;
     const onDeleteTodo = this.props.onDeleteTodo;
     const onUpdateTodo = this.props.onUpdateTodo;
 
+    const searchtext = this.state.searchtext;
+    // const todos = this.props.todos;
+    const todos = searchtext
+      ? this.props.todos.filter(
+      todo => todo.text.indexOf(searchtext) != -1
+      )
+      : this.props.todos;
     return (
       <div>
+        <TodoPanel
+          todos={todos}
+          HandleSearchText={this.HandleSearchText}
+          searchtext={searchtext}
+        />
         {todos.map((todo, index) => {
           return (
-            <div>
-              <TodoPanel
-
-              />
               <TodoListItem
                 key={uniqueId()}
                 todo={todo}
@@ -29,7 +42,6 @@ export default class TodoList extends React.PureComponent {
                 onDeleteTodo={onDeleteTodo}
                 onUpdateTodo={onUpdateTodo}
               />
-            </div>
           );
         })}
       </div>
--- src/containers/TodoPanel.js
+++ src/containers/TodoPanel.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import TextBox from '../components/TextBox';
-import Button from '../components/Button';
 import Radio from '../components/Radio';
 import types from '../constants';
 
 export default class TodoListItem extends React.PureComponent {
   render(){
+    // const searchtext = this.props.searchtext;
+    console.log("sadasd",this.props.todos, this.props.searchtext);
     return(
       <div>
-        привет
+        <TextBox
+          onChange={this.props.HandleSearchText}
+          value={this.props.searchtext}
+        />
       </div>
     )
   }
